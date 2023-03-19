d3e02d139a8649da64974e6a4aa10c4338d37b46
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-03-19 02:00:20 -0300

    Agregando tipos de operadores

---
+++ JS/Estructura de control/Tipos de operadores/operadores_relacionales.js
@@ -0,0 +1,25 @@
+// Devuelve valores booleanos al hacer la comparacion
+
+let igual = 2 == "2";
+console.log(igual); // true
+
+let estrictamenteIgual = 2 === "2";
+console.log(estrictamenteIgual); // false
+
+let desigual = 2 != "2";
+console.log(desigual); // false
+
+let estrictamenteDesigual = 2 !== "2";
+console.log(estrictamenteDesigual); // true
+
+let mayorQue = 4 > 4;
+console.log(mayorQue); // false
+
+let mayorOIgual = 4 >= 4;
+console.log(mayorOIgual); //true
+
+let menorQue = 4 < 4;
+console.log(menorQue); //false
+
+let menorOIgual = 4 <= 4;
+console.log(menorOIgual); // true
