bfd207f139f12173381e9bd62ab033edcc25fbe1
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-04-02 17:33:52 -0400

    DOM agregando Elemento del documento en JS

---
+++ JS/09-DOM/01-Elementos del Documento/elementos_del_documento.js
@@ -0,0 +1,33 @@
+// Capturar todo el documento
+console.log(window.document);
+console.log(document);
+
+// Capturar la etiqueta head
+console.log(document.head);
+// Capturar la etiqueta body
+console.log(document.body);
+// Capturar la etiqueta html
+console.log(document.documentElement);
+
+// Captuturar el title
+console.log(document.title);
+
+// Capturar lista de enlaces
+console.log(document.links);
+
+// Captura lista de imagenes
+console.log(document.images);
+
+// Capturar lista de formularios
+console.log(document.forms);
+
+// Capturar las hojas de estilos
+console.log(document.styleSheets);
+
+// Capturar las scripts de programacion
+console.log(document.scripts);
+
+// Captura lo seleccionado por el usuario
+setTimeout(() => {
+  console.log(document.getSelection().toString());
+}, 3000);
