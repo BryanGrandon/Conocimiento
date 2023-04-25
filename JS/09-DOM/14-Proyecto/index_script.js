4b7803b9d23f234b55d868ed832dabae24067371
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-04-25 10:34:40 -0400

    Agregando funcion de reloj y alarma para el DOM

--- JS/09-DOM/14-Proyecto/index_script.js
+++ JS/09-DOM/14-Proyecto/index_script.js
@@ -1,7 +1,12 @@
 import hamburgerMenu from "./Scripts/hamburger_menu.js";
+import { alarm, digitalClock } from "./Scripts/reloj_alarm.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
+  // Menu Hamburger
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
+  // Reloj Alarm
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+  alarm("assets/alarm.mp3", "#activar-alarm", "#desactivar-alarm");
 });
