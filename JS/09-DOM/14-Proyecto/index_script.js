4e45d59bd6aaa8c5399069f0e37757deb7a4bc4b
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-04-27 18:35:11 -0400

    Agregando dark theme en el proyecto DOM

--- JS/09-DOM/14-Proyecto/index_script.js
+++ JS/09-DOM/14-Proyecto/index_script.js
@@ -1,5 +1,6 @@
 import scrollTopButtton from "./Scripts/boton_scroll.js";
 import countdown from "./Scripts/cuenta_regresiva.js";
+import darkTheme from "./Scripts/dark_theme.js";
 import hamburgerMenu from "./Scripts/hamburger_menu.js";
 import { alarm, digitalClock } from "./Scripts/reloj_alarm.js";
 import { moveBall, shortcuts } from "./Scripts/teclado.js";
@@ -14,8 +15,10 @@ d.addEventListener("DOMContentLoaded", (e) => {
   alarm("assets/alarm.mp3", "#activar-alarm", "#desactivar-alarm");
   // Cuenta regresiva
   countdown("countdown", "December 19, 2023", "Feliz Cumpleaños Bryan Grandon");
-  //
+  // Scroll top
   scrollTopButtton(".scroll-top-btn");
+  // Theme dark
+  darkTheme(".dark-theme-btn", "dark-mode");
 });
 
 d.addEventListener("keydown", (e) => {
