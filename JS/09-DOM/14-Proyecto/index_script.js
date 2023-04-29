89ec82138a46c90e6a6a80144b8641bf3f1f0f96
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-04-29 11:44:20 -0400

    Agregando prueba responsive al proyecto DOM

--- JS/09-DOM/14-Proyecto/index_script.js
+++ JS/09-DOM/14-Proyecto/index_script.js
@@ -3,6 +3,7 @@ import countdown from "./Scripts/cuenta_regresiva.js";
 import darkTheme from "./Scripts/dark_theme.js";
 import hamburgerMenu from "./Scripts/hamburger_menu.js";
 import responsiveMedia from "./Scripts/objeto_responsive.js";
+import responsiveTester from "./Scripts/prueba_responsive.js";
 import { alarm, digitalClock } from "./Scripts/reloj_alarm.js";
 import { moveBall, shortcuts } from "./Scripts/teclado.js";
 
@@ -31,6 +32,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
     `<a href="https://goo.gl/maps/dWqiPdY9cQmYtJ2y8" target="_blank" rel="noopener" a>Ver </a>`,
     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10777.265233320952!2d-70.6384637281991!3d-33.426096242040195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5f2c4b10d51%3A0xf5ed6089dea23c83!2sCerro%20San%20Crist%C3%B3bal!5e0!3m2!1ses!2scl!4v1682691814995!5m2!1ses!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   );
+  // Prueba Responsive
+  responsiveTester("responsive-tester");
 });
 
 d.addEventListener("keydown", (e) => {
