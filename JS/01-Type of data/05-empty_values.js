a88cf133c9c4daf4a2e52a8b557b15068f4b6bc5
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-05 18:08:45 -0400

    update empty_values.js on data type

---
+++ JS/01-Type of data/05-empty_values.js
@@ -0,0 +1,7 @@
+let noValueAssigned;
+console.log(noValueAssigned); // undefined
+
+let isNull = null;
+console.log(isNull); // null
+
+console.log(noValueAssigned == isNull); // true
--- JS/01-Type of data/05-valores_vacios.js
+++
@@ -1,7 +0,0 @@
-let sinValorDesignado;
-console.log(sinValorDesignado); // undefined
-
-let nulo = null;
-console.log(nulo); // null
-
-console.log(sinValorDesignado == nulo); // true
