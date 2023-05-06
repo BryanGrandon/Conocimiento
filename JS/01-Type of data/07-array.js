1d69048344c6120953769c60500478fe37bbefd4
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-06 12:11:28 -0400

    update array.js in data type

--- JS/01-Type of data/07-array.js
+++ JS/01-Type of data/07-array.js
@@ -1,66 +1,68 @@
 const array = [21, true, null, "string"];
 console.log(array);
 
-let elemento1 = array[0];
-console.log(elemento1); // 21
+let element1 = array[0];
+console.log(element1); // 21
 
-let elemento4 = array[3];
-console.log(elemento4); // string
+let element4 = array[3];
+console.log(element4); // string
 
-// PROPIEDADES
+// PROPERTIES
 
-const cantidadDeElementos = [1, 2, 3, 4];
-console.log(cantidadDeElementos.length); // 4
+const fourElements = [1, 2, 3, 4];
+console.log(fourElements.length); // 4
 
-// METODOS
+// METHODS
 
-const agregar = [1, true];
-agregar.push(4);
-console.log(agregar); // [1, true, 4]
+const addElementInArray = [1, true];
+addElementInArray.push(4);
+console.log(addElementInArray); // [ 1, true, 4 ]
 
-const eliminar = [1, true, 4];
-eliminar.pop();
-console.log(eliminar); // [1, true]
+const deleteElementInArray = [1, true, 4];
+deleteElementInArray.pop();
+console.log(deleteElementInArray); // [ 1, true ]
 
-const porcionCompleta = [1, true, 2, false];
-const porcion = porcionCompleta.slice(1, 3);
-console.log(porcion); // [true, 2]
+const arrayFull = [1, true, 2, false];
+const arraySegment = arrayFull.slice(1, 3);
+console.log(arraySegment); // [ true, 2 ]
 
-const integrar = [1, 3];
-integrar.splice(1 /* posicion */, 0 /* elementos */, 2 /* valor */);
-console.log(integrar);
+const elementSplice = [1, 3];
+elementSplice.splice(1 /* position */, 0 /* elements */, 2 /* value */);
+console.log(elementSplice); // [ 1, 2, 3 ]
 
-const arrayInicialMap = [1, 2, 3];
-const arrayFinalMap = arrayInicialMap.map((x) => x + 10);
-console.log(arrayFinalMap); // [11, 12, 13]
+const arrayMapStart = [1, 2, 3];
+const arrayMapEnd = arrayMapStart.map((x) => x + 10);
+console.log(arrayMapEnd); // [ 11, 12, 13 ]
 
-const filtrarArray = [12, 21, 30, 10];
-const filtrado = filtrarArray.filter((x) => x >= 18);
-console.log(filtrado); // [21, 30]
+const arrayFilter = [12, 21, 30, 10];
+const arrayFilterResult = arrayFilter.filter((x) => x >= 18);
+console.log(arrayFilterResult); // [ 21, 30 ]
 
-const iterarLosElemento = [21, 32];
-const iteracion = iterarLosElemento.forEach((e, i) => {
-  console.log(`${e} en el indice ${i}`);
+const forIteration = [21, 32];
+const forEachElement = forIteration.forEach((e, i) => {
+  console.log(`Element ${e} at index ${i}`);
 });
 
-const comprobacion = [2, 4, 6];
-const compruebaUnElemento = comprobacion.some((x) => x > 2);
-console.log(comprobacion); // true
-const compruebaTodosLosElemtos = comprobacion.every((x) => x > 2);
-console.log(compruebaTodosLosElemtos); // false
+const comprobation = [2, 4, 6];
+// If an element satisfies the condition we get true
+const anElement = comprobation.some((x) => x > 2);
+console.log(anElement); // true
+// If all element satidfies the condition we get true
+const allElements = comprobation.every((x) => x > 2);
+console.log(allElements); // false
 
-const muestraElPrimerResultado = [2, 3, 4];
-const primerElemento = muestraElPrimerResultado.find((x) => x > 2);
-console.log(primerElemento); // 3
-const indexDelElemeto = muestraElPrimerResultado.findIndex((x) => x > 2);
-console.log(indexDelElemeto); // 1
+const getElement = [2, 3, 4];
+const firstElementObtained = getElement.find((x) => x > 2);
+console.log(firstElementObtained); // 3
+const indexOfTheFirstElementObtained = getElement.findIndex((x) => x > 2);
+console.log(indexOfTheFirstElementObtained); // 1
 
-const descAsce = [2, 4, 1, 3];
-const ascendente = descAsce.sort((a, b) => (a > b ? 1 : -1));
-console.log(ascendente); // [1,2,3,4]
-const descendente = descAsce.sort((a, b) => (a > b ? -1 : 1));
-console.log(descendente); // [4,3,2,1]
+const order = [2, 4, 1, 3];
+const upwardOrder = order.sort((a, b) => (a > b ? 1 : -1));
+console.log(upwardOrder); // [ 1, 2, 3, 4 ]
+const downwardOrder = order.sort((a, b) => (a > b ? -1 : 1));
+console.log(downwardOrder); // [ 4, 3, 2, 1 ]
 
-const invertirArray = [2, 4, 1, 3];
-const invertir = invertirArray.reverse();
-console.log(invertir); // [3,1,4,2]
+const arrayForInvert = [2, 4, 1, 3];
+const invertedArray = arrayForInvert.reverse();
+console.log(invertedArray); // [ 3, 1, 4, 2 ]
