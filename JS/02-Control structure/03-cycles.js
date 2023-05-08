21fc171b8e31a03c52635475f665b9dad81ab9a3
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-08 16:27:54 -0400

    update cycles.js in control structure

--- JS/02-Control structure/03-cycles.js
+++ JS/02-Control structure/03-cycles.js
@@ -3,24 +3,31 @@
 for (let i = 0; i < 10; i += 2) {
   console.log(i);
 }
-
-const object = {
-  keyA: 21,
-  keyB: "Bryan",
-};
-
-for (key in object) {
-  console.log(key);
-}
-
 const array = ["Bryan", 21];
 
 for (index in array) {
   console.log(index);
 }
-
 for (values of array) {
   console.log(values);
+}
+
+//  break
+
+for (let i = 1; i < 100; i++) {
+  console.log(i);
+  if (i == 5) {
+    break;
+  }
+}
+
+// continue
+
+for (let i = 1; i < 10; i++) {
+  if (i > 3 && i < 6) {
+    continue;
+  }
+  console.log(i);
 }
 
 // WHILE
--- JS/02-Control structure/06-continue.js
+++
@@ -1,8 +0,0 @@
-// Permite saltarnos el codigo restante pero continua con el ciclo
-
-for (let i = 1; i < 6; i++) {
-  if (i === 3) {
-    continue;
-  }
-  console.log(i); // 1, 2, 4, 5
-}
