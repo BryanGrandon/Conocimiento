ea7a70107519371ee12ce2aa507c6e62afbaca48
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-08 17:33:33 -0400

    update spread.js in control structure

---
+++ JS/02-Control structure/07-spread.js
@@ -0,0 +1,11 @@
+// ...iterable element
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+const array3 = [array1, array2];
+console.log(array3);
+// [[1, 2, 3],[4, 5, 6]]
+
+const array4 = [...array1, ...array2];
+console.log(array4); // [1, 2, 3, 4, 5, 6]
