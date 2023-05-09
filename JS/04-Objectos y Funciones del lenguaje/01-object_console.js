610f0133fad3051b8265980a3e488b6c153884b2
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-09 17:08:20 -0400

    update object_console.js

--- JS/04-Objectos y Funciones del lenguaje/01-object_console.js
+++ JS/04-Objectos y Funciones del lenguaje/01-object_console.js
@@ -1,21 +1,19 @@
-// Nos permite mandar informacion
+console.log("display information");
+console.info("informative message");
+console.error("Error message");
+console.warn("Warning");
+console.clear(); // clean console
+console.table(); // create a table
 
-console.log("Mostrar informacion");
-console.info("Mensaje informativo");
-console.error("Mensaje de error");
-console.warn("Advertencia");
-console.clear(); // Limpiar la consola
-console.table(); // crear una tabla
+// count
+console.count("record the times it is called");
+console.countReset("Reset the counter");
 
-// conteo
-console.count("Registra las veces que es llamado");
-console.countReset("Restaura el contador");
+// Group
+console.group(); // create a group
+console.groupEnd(); // group remove
 
-// Agrupacion
-console.group(); // crear un grupo
-console.groupEnd(); // Restaura el contador
-
-// Temporizador
-console.time(); // Inicia un temporizador
-console.timeLog(); // Registra el valor de time
-console.timeEnd(); // Detiene un temporizador
+// Timer
+console.time(); // Start a timer
+console.timeLog(); // Record timer value
+console.timeEnd(); // Stop the timer
