a0a855b1759cfb630608ffc9c12fccbfe7045dfd
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-09 18:56:35 -0400

    update object_math.js

--- JS/04-Language Objects and Functions/03-object_math.js
+++ JS/04-Language Objects and Functions/03-object_math.js
@@ -1,19 +1,45 @@
-// funciones matematicas incorporado
+// METHODS
 
-// Metodos
-let raiz = Math.sqrt(25);
-let raizCubica = Math.cbrt(27);
-let numeroMayor = Math.max(3, 4, 5, 6);
-let numeroMenor = Math.min(-2, 4, 2, 1);
-let numeroRandom = Math.random() * 10;
-let redondeo = Math.round(3.6);
-let eliminarDecimal = Math.trunc(3.9);
+let squareRoot = Math.sqrt(25);
+console.log(squareRoot); // 5
 
-// Propiedades
+let cubeRoot = Math.cbrt(27);
+console.log(cubeRoot); // 3
+
+let maxNumber = Math.max(3, 4, 5, 6);
+console.log(maxNumber); // 6
+
+let minNumber = Math.min(-2, 4, 2, 1);
+console.log(minNumber); // -2
+
+let randomNumber = Math.random() * 10;
+console.log(randomNumber); // 7.8340390766961026
+
+let roundOut = Math.round(3.6);
+console.log(roundOut); // 4
+
+let removeDecimal = Math.trunc(3.9);
+console.log(removeDecimal); // 3
+
+// PROPERTIES
+
 const PI = Math.PI;
-const RAIZ1_2 = Math.SQRT1_2;
-const RAIZ2 = Math.SQRT2;
-const EULER = Math.EULER;
-const LOGARITMO2 = Math.LN2;
-const LOGARITMOE2 = Math.LOG2E;
-const LOGARITMOE10 = Math.LOG10E;
+console.log(PI); // 3.141592653589793
+
+const SQURA_ROOT1_2 = Math.SQRT1_2;
+console.log(SQURA_ROOT1_2); // 0.7071067811865476
+
+const SQUARE_ROOT2 = Math.SQRT2;
+console.log(SQUARE_ROOT2); // 1.4142135623730951
+
+const EULER = Math.E;
+console.log(EULER); // 2.718281828459045
+
+const NATURAL_LOGARITHM2 = Math.LN2;
+console.log(NATURAL_LOGARITHM2); // 0.6931471805599453
+
+const THE_BASE_2_LOGARITHM_EULER = Math.LOG2E;
+console.log(THE_BASE_2_LOGARITHM_EULER); // 1.4426950408889634
+
+const THE_BASE_10_LOGARITHM_EULER = Math.LOG10E;
+console.log(THE_BASE_10_LOGARITHM_EULER); // 0.4342944819032518
