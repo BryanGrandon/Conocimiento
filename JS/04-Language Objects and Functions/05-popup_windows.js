7092b7c748f0de12c98aea2e6d2ba8f5961d38f2
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-10 10:12:16 -0400

    update popup_windows.js

---
+++ JS/04-Language Objects and Functions/05-popup_windows.js
@@ -0,0 +1,11 @@
+// alert
+// warning message
+alert("Text alert");
+
+// confirm
+// option, accept = true, cancel = false
+confirm("Text option");
+
+// prompt
+// text input
+prompt("text prompt");
