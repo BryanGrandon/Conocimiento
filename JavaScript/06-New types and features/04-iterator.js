e07383d6d9f224f9066f2320eeb9287412044e06
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-13 17:18:13 -0400

    update iterator.js

---
+++ JavaScript/06-New types and features/04-iterator.js
@@ -0,0 +1,12 @@
+/* The iterator is an object that allows you to loop through a collection and return a value when done */
+
+const iterable = [1, 2, 3, 4, 5];
+
+const iterador = iterable[Symbol.iterator]();
+
+let next = iterador.next();
+
+while (!next.done) {
+  console.log(next.value);
+  next = iterador.next();
+}
