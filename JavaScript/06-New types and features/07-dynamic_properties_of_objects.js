8c38ccebeef4f94bddc40f364f077a4cf1fa351b
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-15 12:12:32 -0400

    update dynamic_properties_of_objects.js

---
+++ JavaScript/06-New types and features/07-dynamic_properties_of_objects.js
@@ -0,0 +1,13 @@
+/* Dynamic properties in JavaScript are  properties whose names can be dynamically defined and accessd */
+
+let random = Math.round(Math.random() * 100 + 5);
+const objUser = {
+  property: "valor",
+  [`id_${random}`]: "Valor Aleatorio",
+};
+
+const users = ["jon", "bryan", "camila"];
+
+users.forEach((user, index) => (objUser[`id_${index}`] = user));
+
+console.log(objUser);
