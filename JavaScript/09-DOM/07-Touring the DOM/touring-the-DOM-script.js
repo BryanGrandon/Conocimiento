9a67a5bb4d522d3d47d7a13f5ac6b2961a58663a
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-19 16:45:51 -0400

    update Touring-the-DOM in DOM

---
+++ JavaScript/09-DOM/07-Touring the DOM/touring-the-DOM-script.js
@@ -0,0 +1,17 @@
+const $cards = document.querySelector(".cards");
+const cards = {
+  elementBrother: {
+    children: $cards.children,
+    firstElementChild: $cards.firstElementChild,
+    lastElementChild: $cards.lastElementChild,
+  },
+  parentElement: $cards.parentElement,
+  previousElementSibling: $cards.previousElementSibling,
+  nextElementSibling: $cards.nextElementSibling,
+};
+console.log(cards);
+
+let son2 = $cards.children[1];
+
+// Get nearest parent element
+console.log($cards.children[1].closest("section"));
