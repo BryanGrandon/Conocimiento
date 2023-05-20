329c5fb168d69aa315b9ceb52bbd3537ca79530c
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-19 20:23:31 -0400

    updated Templates-HTML in the DOM

--- JavaScript/09-DOM/09-Templates-HTML/templates-html-script.js
+++ JavaScript/09-DOM/09-Templates-HTML/templates-html-script.js
@@ -3,23 +3,23 @@ const $cards = document.querySelector(".cards"),
   $fragment = document.createDocumentFragment(),
   cardsContent = [
     {
-      title: "Tecnologia",
+      title: "Technology",
       img: "https://cdn.pixabay.com/photo/2017/08/10/08/47/laptop-2620118_960_720.jpg",
     },
     {
-      title: "Animales",
+      title: "Animals",
       img: "https://cdn.pixabay.com/photo/2020/06/20/11/08/siamese-cat-5320568_960_720.jpg",
     },
     {
-      title: "Arquitectura",
+      title: "Architecture",
       img: "https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840_960_720.jpg",
     },
     {
-      title: " Gente",
+      title: "People",
       img: "https://cdn.pixabay.com/photo/2017/06/20/22/14/man-2425121_960_720.jpg",
     },
     {
-      title: "Naturaleza",
+      title: "Nature",
       img: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072821_960_720.jpg",
     },
   ];
