2b0c54b3e2b759f97f620b5b3e757950b0e42415
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-22 15:03:36 -0400

    digital clock and alarm added in the projec DOM

--- JavaScript/09-DOM/14-Project/project-script.js
+++ JavaScript/09-DOM/14-Project/project-script.js
@@ -1,7 +1,10 @@
+import { alarm, digitalClock } from "./Functions/clock-alarm.js";
 import hamburgerMenu from "./Functions/hamburger-menu.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
+  digitalClock("#clock", "#active-clock", "#disable-clock");
+  alarm("Assets/alarm.mp3", "#active-alarm", "#disable-alarm");
 });
