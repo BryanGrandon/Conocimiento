4808ce07dd059e6da0e8aaaeb36db1076abcb1b0
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-22 18:26:58 -0400

    countdow added to the DOM project

--- JavaScript/09-DOM/14-Project/project-script.js
+++ JavaScript/09-DOM/14-Project/project-script.js
@@ -1,4 +1,5 @@
 import { alarm, digitalClock } from "./Functions/clock-alarm.js";
+import countdown from "./Functions/countdown.js";
 import hamburgerMenu from "./Functions/hamburger-menu.js";
 import { moveBall, shortcuts } from "./Functions/keyboard-events.js";
 
@@ -8,6 +9,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#clock", "#active-clock", "#disable-clock");
   alarm("Assets/alarm.mp3", "#active-alarm", "#disable-alarm");
+  countdown("countdown", "December 19, 2023", "Happy birthday Bryan Grandon");
 });
 
 // keyboard events
