4c3569315b201c3487ab1e8f1ec4af67b515cfbc
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-23 16:32:25 -0400

    responsive tast added to the DOM project

--- JavaScript/09-DOM/14-Project/project-script.js
+++ JavaScript/09-DOM/14-Project/project-script.js
@@ -3,6 +3,7 @@ import countdown from "./Functions/countdown.js";
 import darkTheme from "./Functions/dark-theme.js";
 import hamburgerMenu from "./Functions/hamburger-menu.js";
 import { moveBall, shortcuts } from "./Functions/keyboard-events.js";
+import responsiveTester from "./Functions/responsive-tester.js";
 import scrollTopButtton from "./Functions/scroll-button.js";
 
 const d = document;
@@ -13,6 +14,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   alarm("Assets/alarm.mp3", "#active-alarm", "#disable-alarm");
   countdown("countdown", "December 19, 2023", "Happy birthday Bryan Grandon");
   scrollTopButtton(".scroll-top-btn");
+  responsiveTester("responsive-tester");
 });
 
 // keyboard events
