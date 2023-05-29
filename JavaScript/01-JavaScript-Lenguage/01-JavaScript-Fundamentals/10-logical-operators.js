f2243b0441374c819641e2b72d85625b0a1f8b55
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-29 17:39:33 -0400

    logical operators added in JavaScript Fundamentals

---
+++ JavaScript/01-JavaScript-Lenguage/01-JavaScript-Fundamentals/10-logical-operators.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// || ( or )
+const or = (value1, value2) => {
+  let result = value1 || value2;
+  console.log(`( ${value1} || ${value2} ) = ${result} `);
+};
+or(true, false); // true
+or(false, false); // false
+
+// && ( and )
+const and = (value1, value2) => {
+  let result = value1 && value2;
+  console.log(`( ${value1} || ${value2} ) = ${result} `);
+};
+and(true, false); // false
+and(true, true); // true
+
+// ! ( not )
+const not = (value) => {
+  let result = !value;
+  console.log(`( !${value} ) = ${result} `);
+};
+not(true); // false
+not(false); // true
