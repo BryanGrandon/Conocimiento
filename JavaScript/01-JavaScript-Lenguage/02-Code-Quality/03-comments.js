8d5c6c5e54f45491f19514f5cce6eee78972f2fe
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-05-30 17:25:44 -0400

    comments added in Code Quality

---
+++ JavaScript/01-JavaScript-Lenguage/02-Code-Quality/03-comments.js
@@ -0,0 +1,26 @@
+// Wrong Comments
+// This code will do this (...) and this (...)
+
+// Correct Comments
+/**
+ * Returns x raised to the power of n.
+ *
+ * @param {number} x The number to raise.
+ * @param {number} n The power.
+ * @return {number} x raised to the power of n.
+ */
+function pow(x, n) {}
+
+/**
+ * Comment This:
+ *
+ *  - General architecture, high level view.
+ *  - Use of functions.
+ *  - Important solutions, especially when they are not immediately obvious.
+ *
+ * Avoid Comments:
+ *
+ *  - They explain “how the code works” and “what it does”.
+ *  - Write them only if it's impossible to write the code so simply and
+ *    self-descriptive that it doesn't need them.
+ */
