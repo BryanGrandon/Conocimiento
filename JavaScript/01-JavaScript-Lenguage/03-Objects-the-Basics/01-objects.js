d5766d92705a5f17b6169739c1bfb12babc6d0b8
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-06-01 09:48:57 -0400

    objects added in Objects the Basics

---
+++ JavaScript/01-JavaScript-Lenguage/03-Objects-the-Basics/01-objects.js
@@ -0,0 +1,36 @@
+"use strict";
+let user1 = new Object(); // object constructor
+let user2 = {}; // object literal
+
+// Literals and properties
+let age = 21;
+
+let user = {
+  key: "value",
+  name: "Bryan",
+  age,
+};
+console.log(user.key);
+console.log(user.age);
+
+// object.keyAdd = value
+user.isAdmin = true;
+
+// Delete object.key
+delete user.age;
+
+// object["key key"] = value
+user["mother name"] = "Cecilia";
+delete user["mother name"];
+
+console.log(user);
+
+// Existing property
+console.log("age" in user); // false
+console.log("name" in user); // true
+
+// For in (loops)
+for (let key in user) {
+  console.log(key);
+  console.log(user[key]);
+}
