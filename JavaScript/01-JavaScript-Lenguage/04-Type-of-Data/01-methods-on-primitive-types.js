1c817beabf38c5d7ae4e27009c49c63a6e2c1dd3
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-06-01 16:03:32 -0400

    methods on primitive types added in Type of Data

---
+++ JavaScript/01-JavaScript-Lenguage/04-Type-of-Data/01-methods-on-primitive-types.js
@@ -0,0 +1,16 @@
+let str1 = "Helllo";
+// .toLowerCase()
+let upperCase = str1.toUpperCase();
+console.log(upperCase); // HELLO
+
+// .toLowerCase()
+let lowerCase = str1.toLowerCase();
+console.log(lowerCase); // hello
+
+// .toFixed()
+let number = 1.23456;
+console.log(number.toFixed(2)); // 1.23
+
+// String to number
+let num = Number("123");
+console.log(num); // 123
