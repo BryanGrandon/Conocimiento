ceec6e4ea889ba774993f722f3a4c451e47dfb27
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-06-09 20:22:06 -0400

    variable scope added in Advenced work with function

---
+++ JavaScript/01-JavaScript-Lenguage/05-Advanced-work-with-functions/03-variable-scope.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// IF, FOR, WHILE
+let message = "Start";
+
+{
+  let message = "Hello";
+  console.log(message); // Hello
+}
+
+{
+  let message = "Goodbye";
+  console.log(message); // Goodbye
+}
+
+console.log(message); // Start
