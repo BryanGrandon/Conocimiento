6d19a90e529b87399e84c78637964bc09fbfe104
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-06-27 08:13:10 -0400

    defineProperty updated in Objects

--- JavaScript/02-Data-Types/Object/defineProperty.js
+++ JavaScript/02-Data-Types/Object/defineProperty.js
@@ -8,40 +8,37 @@ let user = {
 };
 
 // Writable
+
 Object.defineProperty(user, "name", {
-  writable: false,
+  writable: false, // not rename value
 });
-// user.name = "Carlos"; // Error
-console.log(user.name);
 
 // Enumerable
+
 Object.defineProperty(user, "toString", {
-  enumerable: false,
+  enumerable: false, // not iterable
 });
 for (let key in user) console.log(key);
 
 // Configurable
+
 Object.defineProperty(user, "lastName", {
-  configurable: false,
+  configurable: false, // not delete
 });
-user.lastName = "Grandon";
-// delete user.lastName; // Error
 
 // Object.defineProperties
+
 const user_2 = {
   name: "Cecilia",
   lastName: "Correa",
 };
+
 Object.defineProperties(user_2, {
   name: { value: "Cecilia", writable: false },
   lastName: { value: "Correa", writable: false },
 });
-// user_2.name = "Margarita"; // Error
-console.log(user_2);
 
 // Clone object and properties
+
 let clone = Object.defineProperties({}, Object.getOwnPropertyDescriptors(user));
-// clone.name = "Ricardo"; // Error
-clone.lastName = "Correa";
-// delete clone.lastName // Error
 console.log(clone);
